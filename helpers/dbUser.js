668f9b217c7fe18be697cd1f0dc225d479705986
Fynnyx <westerathf@gmail.com>
2022-03-30 10:34:58 +0200

    doesUserExists and changes in write User

--- helpers/dbUser.js
+++ helpers/dbUser.js
@@ -1,18 +1,14 @@
-const { setTimeout } = require("timers")
 const db = require("../handlers/database.js");
 const logger = require("../handlers/logger.js");
 
 exports.writeNewUser = async (id) => {
     try {
         if (id === undefined || id === null && typeof id !== "string") {
-            return console.error("Id not provided");
         }
         const result = await db.query(`INSERT INTO user (userid) VALUES ('${id}')`, { type: db.QueryTypes.INSERT })
             .catch(err => {
                 logger.error(err);
             });
-        console.log(result);
-
     } catch (err) {
         logger.error(err);
     }
@@ -21,7 +17,6 @@ exports.writeNewUser = async (id) => {
 exports.deleteUser = async (id) => {
     try {
         if (id === undefined || id === null && typeof id !== "string") {
-            return console.error("Id not provided");
         }
         await db.query(`DELETE FROM user WHERE userid = '${id}'`, { type: db.QueryTypes.DELETE })
             .then(() => {
@@ -31,6 +26,24 @@ exports.deleteUser = async (id) => {
                 logger.error(err);
             });
 
+    } catch (err) {
+        logger.error(err);
+    }
+}
+
+exports.doesUserExist = async (id) => {
+    try{
+        if (id === undefined || id === null && typeof id !== "string") {
+            return logger.error("Id was provided as undefined, null or not a string");
+        }
+        const result = await db.query(`SELECT * FROM user WHERE userid = '${id}'`, { type: db.QueryTypes.SELECT })
+            .catch(err => {
+                logger.error(err);
+            });
+        if (result == undefined || result.length == 0) {
+            return false;
+        }
+        return true;
     } catch (err) {
         logger.error(err);
     }
