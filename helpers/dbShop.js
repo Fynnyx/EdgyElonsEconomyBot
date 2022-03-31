34458bd15aefcab677971ab406ecc1b751daa28f
Fynnyx <westerathf@gmail.com>
2022-03-31 16:59:12 +0200

    get all shop items

--- helpers/dbShop.js
+++ helpers/dbShop.js
@@ -1,16 +1,10 @@
-const { setTimeout } = require("timers")
 const db = require("../handlers/database.js");
 const logger = require("../handlers/logger.js");
 
-exports.getShopItems = async (name) => {
+exports.getShopItems = async () => {
     try {
-        if (name !== undefined || name !== null && typeof name === "string") {
-            const items = await db.query(`SELECT * FROM shop WHERE name = '${name}'`, { type: db.QueryTypes.SELECT });
-            return items;
-        } else {
-            const items = await db.query(`SELECT * FROM shop`, { type: db.QueryTypes.SELECT });
-            return items;
-        }
+        const items = await db.query(`SELECT * FROM shop`, { type: db.QueryTypes.SELECT });
+        return items;
     } catch (err) {
         logger.error("Error occured while getting shop items" + err);
     }
