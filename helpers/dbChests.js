e39a03d1b5832ab23f90572f9397e571e72bce5c
Fynnyx <westerathf@gmail.com>
2022-03-31 16:59:49 +0200

    get chests

---
+++ helpers/dbChests.js
@@ -0,0 +1,21 @@
+const client = require("../index");
+const db = require("../handlers/database.js");
+const logger = require("../handlers/logger.js");
+
+exports.getChestByName = async (name) => {
+    try {
+        const chest = await db.query(`SELECT * FROM chest WHERE name = ${name}`, { type: db.QueryTypes.SELECT });
+        return chest[0];
+    } catch (err) {
+        logger.error("Error occured while getting chest by name" + err);
+    }
+}
+
+exports.getAllChests = async () => {
+    try {
+        const chests = await db.query(`SELECT * FROM chest`, { type: db.QueryTypes.SELECT });
+        return chests;
+    } catch (err) {
+        logger.error("Error occured while getting all chests" + err);
+    }
+}
