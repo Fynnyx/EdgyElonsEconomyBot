ed8909e2ea860521364c6154cff0330068e5376b
Fynnyx <westerathf@gmail.com>
2022-04-11 12:45:49 +0200

    added db helper

--- commands/shop/shop.js
+++ commands/shop/shop.js
@@ -1,5 +1,6 @@
 const { Client, CommandInteraction, MessageEmbed } = require("discord.js")
 const { getAllChests } = require("../../helpers/dbChests")
+const { getItemsByChestId } = require("../../helpers/dbItems")
 const data = require(`${process.cwd()}/properties.json`)
 
 module.exports = {
@@ -16,20 +17,27 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         const shopEmbed = new MessageEmbed()
-            .setTitle("EdgyElon - Shop")
-            .setDescription("alksdjalksd")
+            .setTitle("Keymaker`s - Chest Shop")
+            .setDescription("This is the Keymaker's shop. You can buy items here.\nYou can buy items by using `/buy <item name>`")
             .setColor(data.style.colors.red)
 
-            const allChests = await getAllChests();
+        const allChests = await getAllChests();
 
-            allChests.forEach(async (chest) => {
-                shopEmbed.addField(chest.name, `${chest.description}\n\n•`, true);
-            })
+        allChests.forEach(async (chest) => {
+            console.log(chest);
+            const items = await getItemsByChestId('1');
+            console.log(items);
+            var itemstring = "";
+            items.forEach(async (item) => {
+                itemstring += `• ${item.name} - ${item.probability}  ${item.type} \n`;
+            });
+            shopEmbed.addField(chest.name, `${chest.description}\n\n${itemstring}`, true);
+        })
 
-            // shopEmbed.addFields(
-            //     { name: "Chest (200 Red Pills)", value: "• Invite - 15%\n• Whitelist - 50%", inline: true },
-            //     { name: "Chest (500 Red Pills)", value: "• Invite - 25%\n• Whitelist - 75%", inline: true },
-            // )
+        // shopEmbed.addFields(
+        //     { name: "Chest (200 Red Pills)", value: "• Invite - 15%\n• Whitelist - 50%", inline: true },
+        //     { name: "Chest (500 Red Pills)", value: "• Invite - 25%\n• Whitelist - 75%", inline: true },
+        // )
 
         await interaction.reply({ embeds: [shopEmbed] })
     }
--- helpers/dbItems.js
+++ helpers/dbItems.js
@@ -1,5 +1,12 @@
 const { setTimeout } = require("timers")
+const db = require("../handlers/database.js");
+const logger = require("../handlers/logger.js");
 
 exports.getItemsByChestId = async (id) => {
-  
+    try {
+        const result = await db.query(`SELECT chestitems.item, chestitems.probability, items.name, items.type , items.value  FROM chestitems LEFT JOIN items ON chestitems.item = items.id WHERE chestitems.chest = ${id}`, { type: db.QueryTypes.SELECT });
+        return result;
+    } catch {
+        logger.error("Error occured while getting items by chest id" + err);
+    }
 }
