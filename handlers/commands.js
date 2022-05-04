011551c34f9bdfa6353f7076536f213ee4a0632b
Fynnyx <westerathf@gmail.com>
2022-05-04 21:59:13 +0200

    remove permission due to api changes

--- handlers/commands.js
+++ handlers/commands.js
@@ -28,40 +28,6 @@ module.exports = async (client) => {
     client.on("ready", async () => {
         // -- Register for a single guild
         const guild = client.guilds.cache.get(data.guildId);
-        await guild.commands.set(arrayOfSlashCommands).then((cmd) => {
-            const getRoles = (commandName) => {
-                const permissions = arrayOfSlashCommands.find(x => x.name === commandName).userPermissions;
-
-                if (!permissions) return null;
-
-                return guild.roles.cache.filter(
-                    (x) => x.permissions.has(permissions) && !x.managed
-                );
-            };
-
-            const fullPermissions = cmd.reduce((accumulator, x) => {
-                const roles = getRoles(x.name)
-                if (!roles) return accumulator;
-
-                const permissions = roles.reduce((a, v) => {
-                    return [
-                        ...a,
-                        {
-                            id: v.id,
-                            type: 'ROLE',
-                            permission: true
-                        }
-                    ];
-                }, []);
-                return [
-                    ...accumulator,
-                    {
-                        id: x.id,
-                        permissions,
-                    }
-                ]
-            }, [])
-            guild.commands.permissions.set({ fullPermissions })
-        });
+        await guild.commands.set(arrayOfSlashCommands)
     });
 }
