881a350121567304b3c9e1896df9ef40fd7ff6a1
Fynnyx <westerathf@gmail.com>
2022-05-04 21:59:24 +0200

    finished buy process

--- commands/shop/buy.js
+++ commands/shop/buy.js
@@ -1,5 +1,9 @@
 const { Client, CommandInteraction, MessageEmbed } = require("discord.js")
 const { buyItem } = require("../../helpers/buy")
+const { getChestByName } = require("../../helpers/dbChests")
+const { getItemsByChestId } = require("../../helpers/dbItems")
+const logger = require("../../handlers/logger")
+const { hasEnoughMoney } = require("../../helpers/dbMoney")
 const data = require(`${process.cwd()}/properties.json`)
 
 module.exports = {
@@ -23,8 +27,35 @@ module.exports = {
      */
 
     run: async (client, interaction, args) => {
-        await interaction.deferReply({ ephemeral: true })
-        const item = args[0]
-        await interaction.followUp({ content: await buyItem(item, interaction.user), ephemeral: true })
+        try {
+            await interaction.deferReply({ ephemeral: true })
+            const item = args[0]
+            const chest = await getChestByName(item)
+            if (chest === undefined || chest === []) {
+                interaction.followUp({ content: `⛔ - This chest does not exist!\nYou used **${item}**`})
+                return
+            }
+            if (chest.isBuyable == 0) {
+                interaction.followUp({ content: `⛔ - This chest is not buyable!\nYou used **${item}**` })
+                return
+            }
+            if (await hasEnoughMoney(interaction.user.id, { redpills: 0, bluepills: chest.bluepills })) {
+                interaction.followUp({ content: `⛔ - You don't have enough money!\nYou need **${chest.bluepills}** blue Pill/s` })
+                return
+            }
+            const items = await getItemsByChestId(chest.id)
+            if (items === undefined || items === []) {
+                interaction.followUp({ content: `⛔ - This chest does not have any items!\nYou used **${item}**` })
+                return
+            }
+            const result = await buyItem(item, interaction.user)
+            if (result == undefined) {
+                return interaction.followUp({ content: `⛔ - Something went wrong while buying the item` })
+            }
+            await interaction.followUp({ files: [`./assets/chests/${result}.gif`], ephemeral: true })
+        } catch (err) {
+            await interaction.followUp({ content: "Something went wrong while buying the item.", ephemeral: true })
+            logger.error(err)
+        }
     }
 }
--- helpers/buy.js
+++ helpers/buy.js
@@ -3,7 +3,8 @@ const client = require("../index.js");
 const data = require(`${process.cwd()}/properties.json`)
 const { getChestByName } = require("../helpers/dbChests.js");
 const { getItemsByChestId } = require("./dbItems.js");
-const { addMoneyToUser } = require("./dbMoney.js");
+const { addMoneyToUser, removeMoneyFromUser } = require("./dbMoney.js");
+const logger = require("../handlers/logger.js");
 
 exports.getItemOnProbability = async (items) => {
     var percent
@@ -30,17 +31,14 @@ exports.redeemItem = async (item, user) => {
     switch (item.type) {
         case "REDPILL":
             await addMoneyToUser(user.id, { redpill: item.amount, bluepill: 0 })
-            return `You got **${item.amount}** Red Pill(s)`
             break;
         case "BLUEPILL":
             await addMoneyToUser(user.id, { redpill: 0, bluepill: item.amount })
-            return `You got **${item.amount}** Blue Pill(s)`
             break;
         case "ROLE":
             const role = client.guilds.cache.get(data.guildId).roles.cache.find(role => role.id === item.value)
             const member = await client.guilds.cache.get(data.guildId).members.fetch(user.id)
             member.roles.add(role)
-            return `You got the **<@&${role.id}>** role`
             break;
         case "ETH":
 
@@ -49,19 +47,14 @@ exports.redeemItem = async (item, user) => {
             const guild = client.guilds.cache.get(data.guildId)
             const channel = guild.channels.cache.find(channel => channel.id === data.channels.invite)
             const invite = await channel.createInvite({ maxAge: 0, maxUses: 1 })
-            try {
-                await user.send(invite.url)
-                return `You got the invite in your DM's`
-            } catch {
-                return "Can not send you an invite!"
-            }
+            await user.send(invite.url)
             break;
         default:
-            return null;
+        // return null;
     }
 }
 
-exports.sendCheckoutMessage = async (item, chest,  user) => {
+exports.sendCheckoutMessage = async (item, chest, user) => {
     const checkoutChannel = await client.channels.fetch(data.channels.checkout)
 
     var checkoutEmbed = new MessageEmbed()
@@ -74,20 +67,16 @@ exports.sendCheckoutMessage = async (item, chest,  user) => {
 
 exports.buyItem = async (item, user) => {
     const chest = await getChestByName(item)
-    if (chest === undefined || chest === []) {
-        return `⛔ - This chest does not exist!\nYou used **${item}**`
-    }
-    if (chest.isBuyable == 0) {
-        return `⛔ - This chest is not buyable!\nYou used **${item}**`
-    }
-    const items = await getItemsByChestId(chest.id)
-    if (items === undefined || items === []) {
-        return `⛔ - This chest does not have any items!\nYou used **${item}**`
-    }
-    const chestItem = await this.getItemOnProbability(items)
+    const chestItem = await this.getItemOnProbability(await getItemsByChestId(chest.id))
 
     await this.sendCheckoutMessage(chestItem, chest, user)
-
-    return this.redeemItem(chestItem, user)
+    try {
+    await this.redeemItem(chestItem, user)
+    await removeMoneyFromUser(user.id, { redpill: 0, bluepill: chest.bluepills })
+    return chestItem.gif
+    } catch (err) {
+        console.log("Couldn't send message");
+        return undefined;
+    }
 }
 
