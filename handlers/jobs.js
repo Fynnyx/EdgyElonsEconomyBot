d066f5fb3e9c588073395ad8d6d76952052e24a8
Fynnyx <westerathf@gmail.com>
2022-05-08 15:46:43 +0200

    add cron jobs to run sceduled events

--- handlers/jobs.js
+++ handlers/jobs.js
@@ -4,18 +4,25 @@ const { sleep } = require('../helpers/sleep.js');
 const data = require(`${process.cwd()}/properties.json`);
 
 
-cron.schedule("8 * * * *", function resetDailyChest() {
+cron.schedule("0 8 * * *", function resetDailyChest() {
+    // console.log("Running a task every day at 8:00");
     console.log("Running a task every day at 8:00");
+},{
+    timezone: "Europe/Zurich"
 })
 
-cron.schedule("8 * * * *", async function shop() {
+cron.schedule("0 13 * * *", async function shop() {
     await openShop();
     await sleep(data.shop.durationMin * 60);
     await closeShop();
+},{
+    timezone: "Europe/Zurich"
 })
 
-cron.schedule("18 * * * *", async function shop() {
+cron.schedule("0 18 * * *", async function shop() {
     await openShop();
     await sleep(data.shop.durationMin * 60);
     await closeShop();
+},{
+    timezone: "Europe/Zurich"
 })
