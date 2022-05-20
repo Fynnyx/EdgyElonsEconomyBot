338a1a5168494350eb6cd63ac3261169cc37ef7d
Fynnyx <westerathf@gmail.com>
2022-05-20 11:49:00 +0200

    shop now realy opens

--- helpers/shop.js
+++ helpers/shop.js
@@ -6,14 +6,16 @@ const variables = require("../variables.json")
 
 exports.openShop = async () => {
     variables.isShopOpen = true;
+    writeFileSync(`${process.cwd()}/variables.json`, JSON.stringify(variables, null, 2));
     const openShopEmbed = new MessageEmbed()
         .setTitle("Let the harvest begin!")
         .setDescription(`You have ${data.shop.durationMin} Minutes until it ends!`)
         .setColor(data.style.colors.red)
         .setThumbnail(client.user.avatarURL)
         .setTimestamp()
-    
-        client.channels.cache.get(data.channels.notifications).send({ content: "@everyone", embeds: [openShopEmbed] })
+        const shopinfoChannel = client.channels.cache.get(data.channels.notifications)
+        await shopinfoChannel.bulkDelete(100)
+        await shopinfoChannel.send({ content: "@everyone", embeds: [openShopEmbed] })
 }
 
 exports.closeShop = async () => {
