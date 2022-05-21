82e279b836a1ab898bc9c4ebdff5077be7c5856b
Fynnyx <westerathf@gmail.com>
2022-05-21 23:57:16 +0200

    fixed unlimited daily chests

--- helpers/dailyreward.js
+++ helpers/dailyreward.js
@@ -5,7 +5,7 @@ const { buyItem } = require("./buy");
 exports.getDailyReward = async (user) => {
     try {
         const result = await buyItem("Daily Chest", user)
-        if (result == undefined) {
+        if (result != undefined) {
             await db.query(`UPDATE user SET hasdailychest = 1 WHERE userid = '${user.id}'`), { type: db.QueryTypes.UPDATE };
         }
         return result
